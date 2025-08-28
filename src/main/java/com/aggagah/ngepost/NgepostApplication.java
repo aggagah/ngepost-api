25b27e466c24193598fb483d75bc1ee3793c58d5
gagah_aji <gagah@stellardata.ai>
2025-08-29 00:53:00 +0700

    feat: initialize repo

---
+++ src/main/java/com/aggagah/ngepost/NgepostApplication.java
@@ -0,0 +1,13 @@
+package com.aggagah.ngepost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgepostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgepostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aggagah/ngepost/NgepostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aggagah.ngepost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgepostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
